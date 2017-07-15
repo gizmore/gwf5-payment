87033944498cfffb7a7f55bd38023f5d8cd6e0f2
gizmore <gizmore@wechall.net>
2017-07-15 04:05:13 +0200

    sync

--- lang/payment_en.php
+++ lang/payment_en.php
@@ -1,5 +1,8 @@
 <?php
 return array(
+
+#############################################################
+'price' => 'Price',
 #############################################################
 'err_order' => 'Your order could not been found.',
 'err_orderable' => 'Please choose an item to purchase first.',
