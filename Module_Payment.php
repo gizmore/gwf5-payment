0aff448feaebbc5c619a029a18548c5f5b15864d
gizmore <gizmore@wechall.net>
2017-07-29 02:33:51 +0200

    backup-working

--- Module_Payment.php
+++ Module_Payment.php
@@ -1,10 +1,11 @@
 <?php
+include 'GDO_Money.php';
 include 'GWF_PaymentModule.php';
 final class Module_Payment extends GWF_Module
 {
 	public $module_priority = 15;
 	public function href_administrate_module() { return href('Payment', 'Orders'); }
-	public function getClasses() { return ['GDO_Money', 'GDO_PaymentModule', 'GWF_Order', 'GWF_MethodPayment', 'GWF_Orderable', 'Payment_Order']; }
+	public function getClasses() { return ['GDO_PaymentModule', 'GWF_Order', 'GWF_MethodPayment', 'GWF_Orderable', 'Payment_Order']; }
 	public function onLoadLanguage() { $this->loadLanguage('lang/payment'); }
 // 	public function getConfig()
 // 	{
